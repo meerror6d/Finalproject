ce2945bfd5dfcbd99ce0d1b0c2f1c4c64860c2f6
Sadia Afrin <afrin1907037@stud.kuet.ac.bd>
2023-06-30 03:04:03 +0600

    login-signin page,java

--- app/src/main/java/com/example/mry_cardiac_recorder/login_signin.java
+++ app/src/main/java/com/example/mry_cardiac_recorder/login_signin.java
@@ -1,14 +1,98 @@
 package com.example.mry_cardiac_recorder;
 
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.mry_cardiac_recorder.databinding.ActivityLoginSigninBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class login_signin extends AppCompatActivity {
+
+    ActivityLoginSigninBinding binding;
+    FirebaseAuth firebaseAuth;
+    ProgressDialog progressdialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_signin);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        binding=ActivityLoginSigninBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        firebaseAuth=FirebaseAuth.getInstance();
+        progressdialog=new ProgressDialog(this);
+
+        binding.login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email= binding.email2.getText().toString().trim();
+                String pass=binding.password2.getText().toString().trim();
+
+                progressdialog.show();
+
+                firebaseAuth.signInWithEmailAndPassword(email,pass)
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                progressdialog.cancel();
+                                //startActivity(new Intent(login_signin.this,record.class));
+                                Toast.makeText(login_signin.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                progressdialog.cancel();
+                                Toast.makeText(login_signin.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                            }
+                        });
+            }
+        });
+
+        binding.forget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email=binding.email2.getText().toString();
+                progressdialog.setTitle("Sending mail");
+                progressdialog.show();
+
+                firebaseAuth.sendPasswordResetEmail(email)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                progressdialog.cancel();
+                                Toast.makeText(login_signin.this, "E-mail sent!", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                progressdialog.cancel();
+                                Toast.makeText(login_signin.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
+
+        binding.signup2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //startActivity(new Intent(login_signin.this,signup_login.class));
+            }
+        });
+
     }
 }
